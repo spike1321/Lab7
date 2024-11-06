194daa77a9fd80aa49c0715978fa40c31a584303
Павел Каплин <spike1321@yandex.ru>
2024-11-06 22:54:06 +0300

    Добавьте файлы проекта.

---
+++ Lab7/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static double Square(int x, int y, int z)
+        {
+            double p = (x + y + z) / 2;
+            double s = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+            return s;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение x первого треугольника: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение y первого треугольника: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение z первого треугольника: ");
+            int z = int.Parse(Console.ReadLine());
+            double S = Square(x,y,z);
+            Console.Write("Введите значение x второго треугольника: ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение y второго треугольника: ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение z второго треугольника: ");
+            int z1 = int.Parse(Console.ReadLine());
+            double S1 = Square(x1, y1, z1);
+            if (!double.IsNaN(S) && !double.IsNaN(S1))
+            {
+                Console.WriteLine("Площадь первого треуголника: {0:00.00}", S);
+                Console.WriteLine("Площадь второго треуголника: {0:00.00}", S1);
+                if (S > S1)
+                {
+                    Console.WriteLine("Площадь первого треуголника больше второго");
+                }
+                else if (S < S1)
+                {
+                    Console.WriteLine("Площадь второго треуголника больше первого");
+                }
+                else
+                {
+                    Console.WriteLine("Площадь первого треуголника равна площади второго");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректно заданны длины сторон.");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void GetParamsCube(double a, out double s, out double v)
+        {
+            s = 6 * Math.Pow(a, 2);
+            v = Math.Pow(a, 3);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба: ");
+            double edgelength = double.Parse(Console.ReadLine());
+            double s;
+            double v;
+            GetParamsCube(edgelength,out s, out v);
+            Console.WriteLine("Площадь куба равна = {0}, объем куба равен = {1}", s,v);
+            Console.ReadKey();
+        }
+    }
+}
